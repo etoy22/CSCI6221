0d38000ff548bebf8f74fc47bfc31bcae459a725
etoy22 <ethanleider@gmail.com>
2023-04-12 15:21:08 -0400

    Test

--- Sceenshot.py
+++ Sceenshot.py
@@ -6,52 +6,41 @@ from pytesseract import pytesseract
 path_to_tesseract = r"Tesseract-OCR\tesseract.exe"
 pytesseract.tesseract_cmd = path_to_tesseract
 # define a callback function to be called when the left mouse button is clicked
-# def on_click(x, y, button, pressed):
-#     global x_pos, y_pos
-#     if button == mouse.Button.left and pressed:
-#         x_pos, y_pos = x,y
-#         # print the position of the mouse when the left button is clicked
-#         print(f"Left button clicked at ({x}, {y})")
-#         return False  # stop listening
+def on_click(x, y, button, pressed):
+    global x_pos, y_pos
+    if button == mouse.Button.left and pressed:
+        x_pos, y_pos = x,y
+        # print the position of the mouse when the left button is clicked
+        print(f"Left button clicked at ({x}, {y})")
+        return False  # stop listening
 
-# x_pos, y_pos = None, None
-# x1,x2,y1,y2 = None,None,None,None
-# # create a listener for mouse events
+x_pos, y_pos = None, None
+x1,x2,y1,y2 = None,None,None,None
+# create a listener for mouse events
 
-# with mouse.Listener(on_click=on_click) as listener:
-#     listener.join()
-#     x1,y1 = x_pos, y_pos
+with mouse.Listener(on_click=on_click) as listener:
+    listener.join()
+    x1,y1 = x_pos, y_pos
 
-# with mouse.Listener(on_click=on_click) as listener:
-#     listener.join()
-#     x2,y2 = x_pos, y_pos
+with mouse.Listener(on_click=on_click) as listener:
+    listener.join()
+    x2,y2 = x_pos, y_pos
 
-# # Double check the quardanates
-# print(f"Region: (left={x1}, top={y1}, right={x2}, bottom={y2}")
+# Double check the quardanates
+print(f"Region: (left={x1}, top={y1}, right={x2}, bottom={y2}")
 
-# # Take quardanates and put them into the region that we will be using 
-# region = (x1, y1, x2, y2)
+# Take quardanates and put them into the region that we will be using 
+region = (x1, y1, x2, y2)
 
-# # Capture the specified region and save it as an image file called screenshot will override image if it already exists
-# image = ImageGrab.grab(bbox=region)
-# image.save("screenshot.png")
+# Capture the specified region and save it as an image file called screenshot will override image if it already exists
+image = ImageGrab.grab(bbox=region)
+image.save("screenshot.png")
 
-# # Open image and then try to convert the image to text 
-# screen = Image.open("screenshot.png")
-# text = pytesseract.image_to_string(screen)
-# print(text[:-1])
+# Open image and then try to convert the image to text 
+screen = Image.open("screenshot.png")
+text = pytesseract.image_to_string(screen)
+print(text[:-1])
 
-import nltk
-nltk.download('stopwords')
-
-# Load the image
-poster_image = Image.open('screenshot.png')
-
-# Extract text from the image using pytesseract
-text = pytesseract.image_to_string(poster_image)
-print(text)
-
-print("____________")
 # Tokenize the text into individual words
 # words = nltk.word_tokenize(text)
 
