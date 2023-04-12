26641658800b841bdb848305018f1d04b3f85f5b
etoy22 <ethanleider@gmail.com>
2023-04-12 11:56:42 -0400

    Add files via upload

--- account.py
+++ account.py
@@ -8,21 +8,30 @@ def addNewAccount(username,password):
 
     #Connect to the database
     conn = sqlite3.connect('Databases/passwords.db')
-
+    c = conn.cursor()
 
     # Hash the password for security
     hashed_password = hashlib.sha256(password.encode()).hexdigest()
 
     # Attempt to login
     try:
-        conn.execute('INSERT INTO users (username, hashed_password) VALUES (?, ?)', (username, hashed_password))
+        c.execute('INSERT INTO users (username, hashed_password) VALUES (?, ?)', (username, hashed_password))
         conn.commit()
+
+
+        c.execute("SELECT id FROM users WHERE username = ?", (username,))
+        id = c.fetchone()
+        conn.close()
+        createNewLibrary(id)
+
+
     except sqlite3.IntegrityError:
         print("Error: Duplicate value for unique column")
         passed = False
+        conn.close()
 
-    # Close the database connection
-    conn.close()
+
+
     return passed
 
 def login(username,password):
@@ -45,17 +54,57 @@ def login(username,password):
     passReturn = row[0]
     hashed_password = hashlib.sha256(password.encode()).hexdigest()
     
-    #Returns True if the username and password match, Returns false if username matches but password does
+    #Returns the id of the user if the username and password match, Returns false if username matches but password does
     if(passReturn==hashed_password):
         print("Username and Password Found")
-        return True
+        c.execute("SELECT id FROM users WHERE username = ?", (username,))
+        return c.fetchone()
     else:
         return False
+
+def createNewLibrary(accNum):
+    conn = sqlite3.connect('Databases/accountLibrary.db')
+    c = conn.cursor()
+    movie_list=[]
+    c.execute("INSERT INTO accounts (accNum, movie_list) VALUES (?, ?)", (accNum, str(movie_list)))
+    conn.commit()
+    conn.close()
+
+def getMovies(accNum):
+    conn = sqlite3.connect('Databases/accountLibrary.db')
+    c = conn.cursor()
+
+    c.execute("SELECT movie_list FROM accounts WHERE accNum = ?", (accNum,))
+
+    rows = c.fetchall()
+
+    movie_lists = []
+
+    for row in rows:
+        movie_list_str = row[0]
+        movie_list = eval(movie_list_str)
+        movie_lists.append(movie_list)
+
+    conn.close()
     
-    
-print("admin passowrd")
-login("admin","password")
-print("---------\nadmin pass")
-login("admin","pass")
-print("---------\nad pass")
-login("ad","password")
+    return movie_list
+
+def addMovies(accNum,movieID):
+    movie_list = getMovies(accNum)
+    movie_list.append(movieID)
+    movie_list_str = str(movie_list)
+
+    conn = sqlite3.connect('Databases/accountLibrary.db')
+    c = conn.cursor()
+    c.execute("UPDATE accounts SET movie_list = ? WHERE accNum = ?", (movie_list_str,accNum))
+    conn.commit()
+    conn.close()
+
+
+
+# print("admin passowrd")
+# login("admin","password")
+# print("---------\nadmin pass")
+# login("admin","pass")
+# print("---------\nad pass")
+# login("ad","password")
--- searching.py
+++ searching.py
@@ -4,26 +4,34 @@ import json
 # Replace YOUR_API_KEY with your TMDb API key
 api_key = '03645002b465428658913956c71ee9e7'
 
-
-
 def movieSearch(movie_query):
+
+    list = []
     # Search by movie
     response = requests.get(f'https://api.themoviedb.org/3/search/movie?api_key={api_key}&query={movie_query}')
 
     # Parse the JSON response
     search_results = json.loads(response.text)
 
-    print("-------")
-    print("Movie Info that is being used")
+    # print("-------")
+    # print("Movie Info that is being used")
     # Print the search results id
     for result in search_results['results']:
-        print("ID:", result['id'],end=" ")
-        print("Title:", result['title'],end=" ")
-        print("Release Date:", result['release_date'],end=" ")
-        print("Overview:", result['overview'])
-        print("----------")
+        # print("ID:", result['id'],end=" ")
+        # print("Title:", result['title'],end=" ")
+        # print("Release Date:", result['release_date'],end=" ")
+        # print("Overview:", result['overview'])
+        my_dict = {}
+        my_dict['ID'] = result['id']
+        my_dict['Title'] = result['title']
+        my_dict['Release Date'] = result['release_date']
+        my_dict['Overview'] = result['overview']
+
+        # print("----------")
+    return list
 
 def actorSearch(actor_name):
+    list = []
     # Make a GET request to the TMDb API search endpoint to get the actor's ID
     response = requests.get(f'https://api.themoviedb.org/3/search/person?api_key={api_key}&query={actor_name}')
     search_results = json.loads(response.text)
@@ -34,21 +42,30 @@ def actorSearch(actor_name):
         response = requests.get(f'https://api.themoviedb.org/3/discover/movie?api_key={api_key}&with_cast={actor_id}')
         discover_results = json.loads(response.text)
 
-        print("Example to analysis")
+        # print("Example to analysis")
         print(discover_results['results'])
 
 
         for result in discover_results['results']:
-            print("ID:", result['id'],end=" ")
-            print("Title:", result['title'],end=" ")
+            my_dict = {}
+            my_dict['ID'] = result['id']
+            my_dict['Title'] = result['title']
+            # print("ID:", result['id'],end=" ")
+            # print("Title:", result['title'],end=" ")
             try:
-                print("Release Date:", result['release_date'],end=" ")
+                # print("Release Date:", result['release_date'],end=" ")
+                my_dict['Release Date'] = result['release_date']
             except:
                 pass
-            print("Overview:", result['overview'])
-            print("----------")
+            # print("Overview:", result['overview'])
+            my_dict['Overview'] = result['overview']
+            # print("----------")
+            list.append(my_dict)
+    return list
 
 def actorMovieSearch(actor_name,movie_title):
+
+    list = []
     # Make a GET request to the TMDb API search endpoint to get the actor's ID
     response = requests.get(f'https://api.themoviedb.org/3/search/person?api_key={api_key}&query={actor_name}')
     search_results = json.loads(response.text)
@@ -64,18 +81,21 @@ def actorMovieSearch(actor_name,movie_title):
         # Filter the movie results by searching for the movie title
         for result in discover_results['results']:
             if movie_title.lower() in result['title'].lower():
-                print("ID:", result['id'],end=" ")
-                print("Title:", result['title'],end=" ")
+                my_dict = {}
+                my_dict['ID'] = result['id']
+                my_dict['Title'] = result['title']
+                # print("ID:", result['id'],end=" ")
+                # print("Title:", result['title'],end=" ")
                 try:
-                    print("Release Date:", result['release_date'],end=" ")
+                    # print("Release Date:", result['release_date'],end=" ")
+                    my_dict['Release Date'] = result['release_date']
                 except:
                     pass
-                print("Overview:", result['overview'])
-                print("----------")
+                # print("Overview:", result['overview'])
+                my_dict['Overview'] = result['overview']
+                list.append(my_dict)
+                # print("----------")
+        return list
 
-# print("Movie Search")
-# movieSearch("matrix")
-# print("Actor Search")
-# actorSearch("keanu")
 print("Movie and Actor Search")
-actorMovieSearch("keanu","matrix")
+temp= actorMovieSearch("keanu","matrix")
